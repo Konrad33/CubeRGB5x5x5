684e97242716ef6f8de5b298ad358f6263503263
rikard789 <41265252+rikard789@users.noreply.github.com>
2021-01-17 19:53:58 +0100

    Update CubeL.c

--- Code/Snake/CubeL.c
+++ Code/Snake/CubeL.c
@@ -37,7 +37,7 @@ static void set0(int x)
 {
 	PTB->PDOR |= (1 << pins[x]);
 	__asm volatile ("nop");__asm volatile ("nop");__asm volatile ("nop");__asm volatile ("nop");__asm volatile ("nop");__asm volatile ("nop");
-	__asm volatile ("nop");__asm volatile ("nop");__asm volatile ("nop");
+	__asm volatile ("nop");__asm volatile ("nop");
 	PTB->PDOR&= ~(1 << pins[x]);
 }
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -158,13 +158,14 @@ void create_line(void * line, int len_start, uint16_t len, uint32_t color)
 {
 	for( int i = len_start; i < (int)(len_start + len); i++)
 	{
-		setPixel(line, len_start,  color );
+		setPixel(line, i,  color );
 	}
 }
 
 
 
 //here we send to our cube what is written in our memory
+
 void send_leds(SRGB * leds, int x)
 {
 	uint8_t i;
@@ -181,7 +182,7 @@ void send_leds(SRGB * leds, int x)
 	{
 		for(i = 25; i < 50; i++ )
 		{
-			sendrgb( *wsk, *(wsk +1), *(wsk +2), 1);
+			sendrgb( *(wsk+75 ), *(wsk+75 +1), *(wsk+75+2), 1);
 			wsk+=3;
 		}
 	}
@@ -189,7 +190,7 @@ void send_leds(SRGB * leds, int x)
 	{
 		for(i = 50; i < 75; i++ )
 		{
-			sendrgb( *wsk, *(wsk +1), *(wsk +2), 2);
+			sendrgb( *(wsk+150), *(wsk+150 +1), *(wsk+150 +2), 2);
 			wsk+=3;
 		}
 	}
@@ -197,7 +198,7 @@ void send_leds(SRGB * leds, int x)
 	{
 		for(i = 75; i < 100; i++ )
 		{
-			sendrgb( *wsk, *(wsk +1), *(wsk +2), 3);
+			sendrgb( *(wsk+225), *(wsk+225 +1), *(wsk+225 +2), 3);
 			wsk+=3;
 		}
 	}
@@ -205,7 +206,7 @@ void send_leds(SRGB * leds, int x)
 	{
 		for(i = 100; i < _LED_CNT; i++ )
 		{
-			sendrgb( *wsk, *(wsk +1), *(wsk +2), 4);
+			sendrgb( *(wsk+300), *(wsk+300 +1), *(wsk+300 +2), 4);
 			wsk+=3;
 		}
 	}
@@ -213,17 +214,16 @@ void send_leds(SRGB * leds, int x)
 }
 
 
-
 /////////////////////////////////////////////////////////////////////////
 // in this function i change between red, green and blue color in the same pattern periodically
 
 void cycle_color_change(	SRGB rgb_leds[] )
 {
-	uint8_t i, k=0;	
+	uint16_t i, k=0;	
 	//uint8_t *wsk = (uint8_t*)&rgb_leds;
 
-	int howmanyleds = 25;                          //here i sets how many led i want to use
-	memset(&rgb_leds,0, 25*3 );										//we must clear all before we begin because when we don't use all leds, leds which are not used can be lgihtened up in diffrent colours
+	uint16_t howmanyleds = 125 ;                          //here i sets how many led i want to use
+	memset(&rgb_leds,0, 125*3 );										//we must clear all before we begin because when we don't use all leds, leds which are not used can be lgihtened up in diffrent colours
 	while(1)
 	{
 		for(int j=0; j<5; j++)
@@ -233,7 +233,6 @@ void cycle_color_change(	SRGB rgb_leds[] )
 			for(i=0; i < howmanyleds*3; i++)  
 			{
 				if((i % 3) == k )*wsk = 255;                              //here we change our colours  
-				
 				*wsk++;
 			}	
 
@@ -241,12 +240,180 @@ void cycle_color_change(	SRGB rgb_leds[] )
 			k++;
 			if(k>2) k=0;
 		}
-		wait(12000000);
+		wait(2400000);
 	}
 
 }
 
 ////////////////////////////////////////////////////////////////////////////
+
+
+void flying_strips1(void * pasek)
+{
+while(1){
+	for(int i = 0; i < 125; i++)
+	{
+	
+	create_line( pasek, i, 4, 0x0f0f00);
+	create_line( pasek, 121 -i, 4, 0x000f0f);
+		for(int j = 0; j < 5; j++)
+		{
+			send_leds(pasek, j);
+		}
+	delay_ms(100);
+	}
+ }
+}
+
+	
+void flying_strips2(void * pasek, int data[5][5][5])
+{
+	//int x,y,z;
+	
+	while(1){
+		//for( int i=0; i < 4; i++)
+		//{
+		//1
+			for( int y=0; y < 5; y++)
+			{
+				//1 strip 
+				setPixel(pasek, data[0][0][4-y], 0x000000);
+				if(y==4) setPixel(pasek, data[0][y-4][0], 0x000000);
+				setPixel(pasek, data[0][y][0], 0x0f0f00);
+				//5 strip 
+				setPixel(pasek, data[4][0][4-y], 0x000000);
+				if(y==4) setPixel(pasek, data[4][y-4][0], 0x000000);
+				setPixel(pasek, data[4][y][0], 0x0f0f00);
+				//4 strip 
+				setPixel(pasek, data[3][0][4-y], 0x000000);
+				if(y==4) setPixel(pasek, data[3][y-4][0], 0x000000);
+				setPixel(pasek, data[3][y][0], 0x00000f);
+				//2 strip 
+				setPixel(pasek, data[1][0][4-y], 0x000000);
+				if(y==4) setPixel(pasek, data[1][y-4][0], 0x000000);
+				setPixel(pasek, data[1][y][0], 0x00000f);
+				//3 strip 
+				setPixel(pasek, data[2][0][4-y], 0x000000);
+				if(y==4) setPixel(pasek, data[2][y-4][0], 0x000000);
+				setPixel(pasek, data[2][y][0], 0x000f0f);
+				
+				
+				send_leds(pasek, 0);
+				send_leds(pasek, 1);
+				send_leds(pasek, 2);
+				send_leds(pasek, 3);
+				send_leds(pasek, 4);
+				delay_ms(500);
+			}
+			//2
+			for( int z=1; z < 5; z++)
+			{
+				//1 strip 
+				setPixel(pasek, data[0][z-1][0], 0x000000);
+				if(z==4) setPixel(pasek, data[0][4][z-4], 0x000000);
+				setPixel(pasek, data[0][4][z], 0x0f0f00);
+				//5 strip 
+				setPixel(pasek, data[4][z-1][0], 0x000000);
+				if(z==4) setPixel(pasek, data[4][4][z-4], 0x000000);
+				setPixel(pasek, data[4][4][z], 0x0f0f00);
+				//4 strip 
+				setPixel(pasek, data[3][z-1][0], 0x000000);
+				if(z==4) setPixel(pasek, data[3][4][z-4], 0x000000);
+				setPixel(pasek, data[3][4][z], 0x00000f);
+				//2 strip
+				setPixel(pasek, data[1][z-1][0], 0x000000);
+				if(z==4) setPixel(pasek, data[1][4][z-4], 0x000000);
+				setPixel(pasek, data[1][4][z], 0x00000f);
+				//3 strip 
+				setPixel(pasek, data[2][z-1][0], 0x000000);
+				if(z==4) setPixel(pasek, data[2][4][z-4], 0x000000);
+				setPixel(pasek, data[2][4][z], 0x000f0f);
+				
+				send_leds(pasek, 0);
+				send_leds(pasek, 1);
+				send_leds(pasek, 2);
+				send_leds(pasek, 3);
+				send_leds(pasek, 4);
+				delay_ms(500);
+			}
+			//3
+			for( int y=3; y >= 0; y--)
+			{
+				//1 strip
+				setPixel(pasek, data[0][4][4-y], 0x000000);
+				if(y==1) setPixel(pasek, data[0][y+4][0], 0x000000);
+				setPixel(pasek, data[0][y][4], 0x0f0f00);
+				//5 strip
+				setPixel(pasek, data[4][4][4-y], 0x000000);
+				if(y==1) setPixel(pasek, data[4][y+4][0], 0x000000);
+				setPixel(pasek, data[4][y][4], 0x0f0f00);
+				//4 strip
+				setPixel(pasek, data[3][4][4-y], 0x000000);
+				if(y==1) setPixel(pasek, data[3][y+4][0], 0x000000);
+				setPixel(pasek, data[3][y][4], 0x00000f);
+				//2 strip
+				setPixel(pasek, data[1][4][4-y], 0x000000);
+				if(y==1) setPixel(pasek, data[1][y+4][0], 0x000000);
+				setPixel(pasek, data[1][y][4], 0x00000f);
+				//3 strip
+				setPixel(pasek, data[2][4][4-y], 0x000000);
+				if(y==1) setPixel(pasek, data[2][y+4][0], 0x000000);
+				setPixel(pasek, data[2][y][4], 0x000f0f);
+				
+				send_leds(pasek, 0);
+				send_leds(pasek, 1);
+				send_leds(pasek, 2);
+				send_leds(pasek, 3);
+				send_leds(pasek, 4);
+				delay_ms(500);
+			}
+			//4
+			for( int z=3; z >= 0; z--)
+			{
+				//1 strip
+				setPixel(pasek, data[0][z][4], 0x000000);
+				if(z==1) setPixel(pasek, data[0][z+4][0], 0x000000);
+				setPixel(pasek, data[0][0][z], 0x0f0f00);
+				//5 strip
+				setPixel(pasek, data[4][z][4], 0x000000);
+				if(z==1) setPixel(pasek, data[4][z+4][0], 0x000000);
+				setPixel(pasek, data[4][0][z], 0x0f0f00);
+				//4 strip
+				setPixel(pasek, data[3][z][4], 0x000000);
+				if(z==1) setPixel(pasek, data[3][z+4][0], 0x000000);
+				setPixel(pasek, data[3][0][z], 0x00000f);
+				//2 strip
+				setPixel(pasek, data[1][z][4], 0x000000);
+				if(z==1) setPixel(pasek, data[1][z+4][0], 0x000000);
+				setPixel(pasek, data[1][0][z], 0x00000f);
+				//3 strip
+				setPixel(pasek, data[2][z][4], 0x000000);
+				if(z==1) setPixel(pasek, data[2][z+4][0], 0x000000);
+				setPixel(pasek, data[2][0][z], 0x000f0f);
+				
+				
+				send_leds(pasek, 0);
+				send_leds(pasek, 1);
+				send_leds(pasek, 2);
+				send_leds(pasek, 3);
+				send_leds(pasek, 4);
+				delay_ms(500);
+			}
+			
+		//}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
 
 //wait function where argument is clock tick
 void wait(volatile uint32_t cnt) {
