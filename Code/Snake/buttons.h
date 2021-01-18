ebd80b1958be05c1044b3035a2c984114b7092e1
rikard789 <41265252+rikard789@users.noreply.github.com>
2021-01-18 18:58:39 +0100

    Update buttons.h

--- Code/Snake/buttons.h
+++ Code/Snake/buttons.h
@@ -7,15 +7,7 @@
 #define BUTTON_3_POS 3
 #define BUTTON_4_POS 4
 
-
-//#define RED_LED_POS 8
-//#define GREEN_LED_POS 9
-//#define BLUE_LED_POS 10
-
-
 void delay_ms( int n);
 void buttonsInitialize(void);
-void buttonsService(void* line);							//buttons' interrupt service procedure
-void PORTB_IRQHandler(void);
 
 #endif
