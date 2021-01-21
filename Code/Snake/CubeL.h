a7e672dbc3cb2129babc213052e7d44478d480a7
rikard789 <41265252+rikard789@users.noreply.github.com>
2021-01-21 22:28:13 +0100

    Update CubeL.h

--- Code/Snake/CubeL.h
+++ Code/Snake/CubeL.h
@@ -1,9 +1,9 @@
 #ifndef CubeL_H
 #define CubeL_H
 
-#define Edgesize 5
-#define _LED_CNT 125
-#define PTB1 6
+#define Edgesize 5	// length of one side of the cube
+#define _LED_CNT 125	//here we define how many leds our cube have (int this project we use 5x5x5 cube so its 125)
+#define PTB1 6        	//here we define which pins we use for our cube
 #define PTB2 7
 #define PTB3 8
 #define PTB4 9
@@ -22,28 +22,26 @@ typedef struct{
 static int pins[Edgesize] = {PTB1,PTB2,PTB3,PTB4,PTB5};
 
 
-void InitializeCube();
-void sendrgb(uint8_t r, uint8_t g, uint8_t b, int x );
-void sendcolor(uint32_t c, int x );
-void clearwall(int x);
-void lightupwall(int x);
-void clear_all(void);
+void InitializeCube();                     //this function prepares pins of our FRDM-KL05Z to  sending data to our cube             
+void sendrgb(uint8_t r, uint8_t g, uint8_t b, int x );   //on subsequent calls it lights up leds on selected pin in color of choice starting from beggining of the line
+void sendcolor(uint32_t c, int x );		//on subsequent calls it lights up leds on selected pin in color written in hex starting from beggining of the line
+void clearwall(int x);				//this function sends bits 0 on selected pin which causes leds to go out
+void lightupwall(int x);			//this function sends bits 1 on selected pin which causes leds to light up  
+void clear_all(void);				//this function sends to all of our pins bits 0 which causes leds to go out
 
-void clearAllPixels(void * line );
-void setPixel( void * line, int ledofchoice, uint32_t color );
-void create_line(void * line, int len_start, uint16_t len, uint32_t color);
-void send_leds(SRGB * leds, int x);
+void clearAllPixels(void * line );			// this function is setting color of all leds to 0 in the array of structures
+void setPixel( void * line, int ledofchoice, uint32_t color );                 // with this we set selected color on led of our choice in the array of structures
+void create_line(void * line, int len_start, uint16_t len, uint32_t color);    //with this we create stripe with specified length and position on the cube and color
+void send_leds(SRGB * leds, int x);        //with this we are sending whole array of structures to the cube
 
 void wait(volatile uint32_t cnt);
 
 
 //effects
-void cycle_color_change( SRGB rgb_leds[] );
-void flying_strips1(void * pasek);
-void flying_strips2(void * pasek, int data[5][5][5]);
-
-
-
+void cycle_color_change( SRGB rgb_leds[] );        		// walls of the cube change colors in sequence red,green and blue
+void flying_strips1(void * pasek);				// 2 stripes are flying in the cube leaving trace of lit up leds
+void flying_strips2(void * pasek);				// 2 stripes which fly in the cube
+void flying_strips2(void * pasek, int data[5][5][5]);		// stripes in diffrent colors which are circling on the sides of the cube
 
 
 
